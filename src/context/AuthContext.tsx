8bf2712a27836c309e139caae2f46001e10e3b08
Jake Quinter <hello@jakequinter.io>
2023-07-21 18:01:21 -0400

    auth state

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,48 +1,64 @@
-import { createContext, createEffect, JSX } from 'solid-js';
+import { createContext, createEffect, JSX, onCleanup } from 'solid-js';
 import { createStore } from 'solid-js/store';
+import { useNavigate } from '@solidjs/router';
 
 type AuthState = {
-  email: string;
-  token: string;
+  token: string | null;
 };
 
 type AuthActions = {
-  setEmail: (email: string) => void;
   setToken: (token: string) => void;
-  login: (email: string, token: string) => void;
+  login: (email: string, apiKey: string) => void;
 };
 
 type AuthContextValue = [AuthState, AuthActions];
 
 export const AuthContext = createContext<AuthContextValue>([
-  { email: '', token: '' },
+  { token: '' },
   {
-    setEmail: () => {},
     setToken: () => {},
     login: () => {},
   },
 ]);
 
 export function AuthProvider(props: { children: JSX.Element }) {
-  const [state, setState] = createStore<AuthState>({ email: '', token: '' });
+  const navigate = useNavigate();
+  const [state, setState] = createStore<AuthState>({
+    token: localStorage.getItem('token') || null,
+  });
 
   createEffect(() => {
-    // TODO: figure out logic when we get there
-    const email = localStorage.getItem('email');
-    const token = localStorage.getItem('token');
+    if (!state.token) {
+      navigate('/login');
+      return;
+    }
+
+    navigate('/');
+  }, [state.token]);
+
+  createEffect(() => {
+    const handleCookieChange = () => {
+      const newToken = localStorage.getItem('token');
+      if (!newToken) {
+        navigate('/login');
+      }
+    };
+
+    window.addEventListener('storage', handleCookieChange);
+    onCleanup(() => {
+      window.removeEventListener('storage', handleCookieChange);
+    });
   });
 
   const user: AuthContextValue = [
     state,
     {
-      setEmail: (email: string) => {
-        setState('email', email);
-      },
       setToken: (token: string) => {
         setState('token', token);
       },
-      login: (email: string, token: string) => {
-        localStorage.setItem('email', email);
+      login: (email: string, apiKey: string) => {
+        const token = btoa(`${email}:${apiKey}`);
+        setState('token', token);
         localStorage.setItem('token', token);
       },
     },
