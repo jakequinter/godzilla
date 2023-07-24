96acf76870004d9ac45b1f255dc1a634896bf945
Jake Quinter <hello@jakequinter.io>
2023-07-24 18:59:08 -0400

    remove test query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,8 @@
 import { A } from '@solidjs/router';
-import { createQuery } from '@tanstack/solid-query';
 
 import Container from '../components/Container';
 
 export default function Home() {
-  const query = createQuery(
-    () => ['repoData'],
-    async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      return response.json();
-    }
-  );
-
-  console.log('query', query.data);
-
   return (
     <Container>
       <h1 class="text-3xl font-semibold text-violet-600">Home</h1>
