0104c593364765bd726165423e680ea6c4ab49ae
Jake Quinter <hello@jakequinter.io>
2023-07-26 14:54:29 -0400

    react

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,8 @@
-import { For, useContext } from 'solid-js';
-import { A, useLocation } from '@solidjs/router';
-import { createQuery } from '@tanstack/solid-query';
+import { CheckCircle, Diamond, Notepad } from '@phosphor-icons/react';
+import { useQuery } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api/tauri';
-import { CheckCircle, Diamond, Notepad } from 'phosphor-solid';
+import { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { AuthContext } from '../context/AuthContext';
 import { Board } from '../types/board';
@@ -23,60 +23,56 @@ const routes = [
 
 export default function Nav() {
   const { pathname } = useLocation();
-  const [{ token, jiraInstance }] = useContext(AuthContext);
+  const { token, jiraInstance } = useContext(AuthContext);
 
-  const query = createQuery<Board>(
-    () => ['projects'],
-    async () => {
+  const { data, isLoading } = useQuery<Board>({
+    queryKey: ['projects'],
+    queryFn: async () => {
       const data = await invoke<Board>('fetch_boards', { jiraInstance, token });
       return data;
-    }
-  );
+    },
+  });
 
   return (
-    <div class="flex h-screen flex-grow flex-col overflow-y-auto bg-white">
-      <h1 class="py-4 text-center text-3xl font-bold text-violet-700">godzilla</h1>
+    <div className="flex h-screen flex-grow flex-col overflow-y-auto bg-white">
+      <h1 className="py-4 text-center text-3xl font-bold text-violet-700">godzilla</h1>
 
-      <div class="flex flex-grow flex-col">
-        <nav class="mt-2.5 px-2" aria-label="Sidebar">
-          <ul class="space-y-1">
-            <For each={routes}>
-              {route => (
-                <li>
-                  <A
-                    href={route.path}
-                    class={cn(
-                      pathname === route.path ? `bg-gray-100 text-gray-900 ` : '',
-                      'group relative flex w-full cursor-default items-center space-x-2 rounded-md p-1 text-left text-sm font-medium hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-0'
-                    )}
-                  >
-                    <span>{route.icon}</span>
-                    <h2>{route.name}</h2>
-                  </A>
-                </li>
-              )}
-            </For>
+      <div className="flex flex-grow flex-col">
+        <nav className="mt-2.5 px-2" aria-label="Sidebar">
+          <ul className="space-y-1">
+            {routes.map(route => (
+              <li key={route.name}>
+                <Link
+                  to={route.path}
+                  className={cn(
+                    pathname === route.path ? `bg-gray-100 text-gray-900 ` : '',
+                    'group relative flex w-full cursor-default items-center space-x-2 rounded-md p-1 text-left text-sm font-medium hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-0'
+                  )}
+                >
+                  <span>{route.icon}</span>
+                  <h2>{route.name}</h2>
+                </Link>
+              </li>
+            ))}
           </ul>
 
-          <div class="mt-6 text-sm">
-            <h3 class="mb-2 text-xs">Projects</h3>
-            <ul class="space-y-1">
-              <For each={query.data?.values}>
-                {board => (
-                  <li>
-                    <A
-                      href="/"
-                      class={cn(
-                        pathname === '/about' ? `bg-gray-100 text-gray-900 ` : '',
-                        'group relative flex w-full cursor-default items-center space-x-2 rounded-md p-1 text-left font-medium hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-0'
-                      )}
-                    >
-                      <Diamond weight="fill" class="text-gray-900" />
-                      <h2>{board.name}</h2>
-                    </A>
-                  </li>
-                )}
-              </For>
+          <div className="mt-6 text-sm">
+            <h3 className="mb-2 text-xs">Projects</h3>
+            <ul className="space-y-1">
+              {data?.values.map(board => (
+                <li key={board.id}>
+                  <Link
+                    to="/"
+                    className={cn(
+                      pathname === '/about' ? `bg-gray-100 text-gray-900 ` : '',
+                      'group relative flex w-full cursor-default items-center space-x-2 rounded-md p-1 text-left font-medium hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-0'
+                    )}
+                  >
+                    <Diamond weight="fill" className="text-gray-900" />
+                    <h2>{board.name}</h2>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
-import { A } from '@solidjs/router';
-
-import Container from '../components/Container';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <Container>
-      <h1 class="text-3xl font-semibold text-violet-600">Home</h1>
-      <A href="/about">go to about</A>
-    </Container>
+    <div>
+      <h1 className="text-3xl font-semibold text-violet-600">Home</h1>
+      <Link to="/about">go to about</Link>
+    </div>
   );
 }
